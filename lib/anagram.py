d339b4b8d5617ab3a444b4bc50ee9676f68632dc
norahkibathi <norah.njeri@student.moringaschool.com>
2024-03-18 14:31:02 +0300

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, word_list):
+        anagrams = []
+        for test_word in word_list:
+            if sorted(self.word.lower()) == sorted(test_word.lower()) and self.word.lower() != test_word.lower():
+                anagrams.append(test_word)
+        return anagrams
